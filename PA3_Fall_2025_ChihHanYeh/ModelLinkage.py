41e6735e94dc95b9fda39202bea7c1a0a6eb2800
Josh CH Yeh <jy0825@bu.edu>
2025-10-27 06:40:24 -0400

    Predator

--- PA3_Fall_2025_ChihHanYeh/ModelLinkage.py
+++ PA3_Fall_2025_ChihHanYeh/ModelLinkage.py
@@ -53,8 +53,8 @@ except ImportError:
 #         3. The predator and prey should have distinguishable different colors.
 #         4. You are welcome to reuse your PA2 creature in this assignment.
 
-class TadpolePrey(Component, EnvironmentObject):
-    "Simple prey creature with the appearance of a tadpole"
+class Prey(Component, EnvironmentObject):
+    "Prey: Creature with the appearance of a tadpole"
     
     def __init__(self, parent, position, shaderProg):
         super().__init__(position)  # Call Component's init
@@ -120,6 +120,102 @@ class TadpolePrey(Component, EnvironmentObject):
 
         self.update() # Apply transformations
 
+class Predator(Component, EnvironmentObject):
+    "Predator: Similar to prey but green and have moving pincers"
+    def __init__(self, parent, position, shaderProg):
+        super().__init__(position)  # Call Component's init
+        self.contextParent = parent
+        self.species_id = 1         # ID for Prey
+        
+        # Setting colors
+        color_body = Ct.ColorType(0, 1, 0)      # Bright green
+        color_tail = Ct.ColorType(0.8, 0.9, 0)  # Soft Green
+        color_pincer = Ct.ColorType(0, 0.4, 0)  # Dark Green
+        
+        # Setting shapes for the creature
+        # Main body -- Parent
+        body_size = [0.5, 0.5, 0.7]     # Larger than prey
+        self.body = Sphere(Point((0, 0, 0)), shaderProg, body_size, color_body, limb=False)
+        self.addChild(self.body)
+
+        # Tail
+        # Segment 1 - Moving cylinder Attached to the back of the body
+        tail_s1_size = [0.1, 0.1, 0.5]
+        self.tail_s1 = Cylinder(Point((0, 0, -body_size[2]/2)), shaderProg, tail_s1_size, color_tail)
+        self.body.addChild(self.tail_s1)
+        # Segment 2 - Cone tip attached to segment 1
+        tail_s2_size = [0.1, 0.1, 0.25] # Cone: radius, radius, length
+        self.tail_s2 = Cone(Point((0, 0, tail_s1_size[2])), shaderProg, tail_s2_size, color_tail)
+        self.tail_s1.addChild(self.tail_s2)
+
+        # Pincer
+        pincer_s1_size = [0.08, 0.08, 0.4]      # Cylinder part
+        pincer_s2_size = [0.08, 0.08, 0.2]      # Cone tip
+        pincer_attach_z = body_size[2]/2 - 0.1  # Attach near the front
+        pincer_attach_x = body_size[0]/2        # Attach to the sides
+        
+        # Right Pincer
+        self.pincer_r1 = Cylinder(Point((pincer_attach_x, 0, pincer_attach_z)), shaderProg, pincer_s1_size, color_limbs)
+        self.body.addChild(self.pincer_r1)
+        self.pincer_r2 = Cone(Point((0, 0, pincer_s1_size[2])), shaderProg, pincer_s2_size, color_pincer)
+        self.pincer_r1.addChild(self.pincer_r2)
+
+        # Left Pincer (Mirrored)
+        self.pincer_l1 = Cylinder(Point((-pincer_attach_x, 0, pincer_attach_z)), shaderProg, pincer_s1_size, color_limbs)
+        self.body.addChild(self.pincer_l1)
+        self.pincer_l2 = Cone(Point((0, 0, pincer_s1_size[2])), shaderProg, pincer_s2_size, color_pincer)
+        self.pincer_l1.addChild(self.pincer_l2)            
+        
+        # Components Storage
+        self.tail_segments = [self.tail_s1, self.tail_s2]
+        self.right_pincer = [self.pincer_r1, self.pincer_r2]
+        self.left_pincer = [self.pincer_l1, self.pincer_l2]
+        # Include ALL parts for animation and selection
+        self.components = [self.body] + self.tail_segments + self.right_pincer + self.left_pincer
+        self.componentList = self.components # For Sketch.py
+        self.componentDict = {
+            "body": self.body,
+            "tail_s1": self.tail_s1, "tail_s2": self.tail_s2,
+            "pincer_r1": self.pincer_r1, "pincer_r2": self.pincer_r2,
+            "pincer_l1": self.pincer_l1, "pincer_l2": self.pincer_l2
+        }
+
+        # Animation & Collision Setup
+        self.tail_wiggle_speed = 3.5
+        self.pincer_snap_speed = 2.0
+        self.translation_speed = Point([random.uniform(-0.015, 0.015) for _ in range(3)])
+        self.bound_center = Point((0,0,0))
+        self.bound_radius = body_size[2] * 1.1 # Based on body length
+
+        # Set Pose and Limits
+        self.setJointLimits()
+        self.setDefaultPose()
+
+    def setJointLimits(self):
+        self.tail_s1.setRotateExtent(self.tail_s1.vAxis, -45, 45) # Wiggle L/R
+        self.pincer_r1.setRotateExtent(self.pincer_r1.vAxis, -10, 30) # Open/Close R
+        self.pincer_l1.setRotateExtent(self.pincer_l1.vAxis, -30, 10) # Open/Close L (mirrored)
+
+    def setDefaultPose(self):
+        self.tail_s1.setDefaultAngle(0, self.tail_s1.vAxis)
+        self.pincer_r1.setDefaultAngle(15, self.pincer_r1.vAxis) # Pincers start slightly open
+        self.pincer_l1.setDefaultAngle(-15, self.pincer_l1.vAxis)
+
+    def animationUpdate(self):
+        # Wiggle tail
+        self.tail_s1.rotate(self.tail_wiggle_speed, self.tail_s1.vAxis)
+        if self.tail_s1.vAngle in self.tail_s1.vRange:
+             self.tail_wiggle_speed *= -1
+
+        # Snap pincers
+        self.pincer_r1.rotate(self.pincer_snap_speed, self.pincer_r1.vAxis)
+        self.pincer_l1.rotate(-self.pincer_snap_speed, self.pincer_l1.vAxis) # Mirrored
+        # Reverse both if one hits limit
+        if self.pincer_r1.vAngle in self.pincer_r1.vRange or \
+           self.pincer_l1.vAngle in self.pincer_l1.vRange:
+             self.pincer_snap_speed *= -1
+
+        self.update()
 class Linkage(Component, EnvironmentObject):
     """
     A Linkage with animation enabled and is defined as an object in environment
