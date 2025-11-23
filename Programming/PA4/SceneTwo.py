004e2ef1fe007d403f323bcac8802348ce67e9fe
Josh CH Yeh <jy0825@bu.edu>
2025-11-23 16:37:29 -0500

    Editing SceneTwo

--- Programming/PA4/SceneTwo.py
+++ Programming/PA4/SceneTwo.py
@@ -1,12 +1,9 @@
 """
-Define a fixed scene with rotating lights
-First version in 11/08/2021
+Second scene for TODO5
 
-:author: micou(Zezhou Sun)
-:version: 2025.11.11
+Shapes: Ellipsoid, Torus, Cylinder
 """
 import math
-
 import numpy as np
 
 import ColorType
@@ -17,18 +14,16 @@ from Material import Material
 from Point import Point
 import GLUtility
 
-from DisplayableCube import DisplayableCube
 from DisplayableEllipsoid import DisplayableEllipsoid
-from DisplayableSphere import DisplayableSphere
 from DisplayableCylinder import DisplayableCylinder
 from DisplayableTorus import DisplayableTorus
 
-class SceneOne(Scene):
+
+class SceneTwo(Scene):
     lights = None
     lightCubes = None
     shaderProg = None
     glutility = None
-
 
     def __init__(self, shaderProg):
         super().__init__(Point((0, 0, 0)))
@@ -37,13 +32,30 @@ class SceneOne(Scene):
 
         # ambient lighting contribution from the scene
         self.sceneAmbient = np.array([0.1, 0.1, 0.1])
+        
+        # Gold Ellipsoid
+        ellipsoid = Component(Point((-1.0, 0.5, 0)), DisplayableEllipsoid(shaderProg, 0.5, 0.6, 0.7, color=ColorType.YELLOW))
+        m_gold = Material(
+            np.array([0.25, 0.22, 0.06, 1.0]),  # Ambient
+            np.array([0.35, 0.31, 0.09, 1.0]),  # Diffuse
+            np.array([0.8, 0.7, 0.2, 1.0]),  # Specular
+            83
+            )
+        ellipsoid.setMaterial(m_gold)
+        ellipsoid.renderingRouting = "lighting"
+        self.addChild(ellipsoid)
 
-        cube = Component(Point((-0.75, 0, -0.75)), DisplayableCube(shaderProg, 1.0))
-        m1 = Material(np.array((0.0, 0.05, 0.1, 1.0)), np.array((0, 0.3, 0.6, 1)),
-                      np.array((0, 0.3, 0.6, 1.0)), 64)
-        cube.setMaterial(m1)
-        cube.renderingRouting = "lighting"
-        self.addChild(cube)
+        # Emeral Cylinder
+        cylinder = Component(Point((1.0, 1.0, 0)), DisplayableCylinder(shaderProg, radius=0.5, height=2, color=ColorType.GREEN))
+        m_gold = Material(
+            np.array([0.25, 0.22, 0.06, 1.0]),  # Ambient
+            np.array([0.35, 0.31, 0.09, 1.0]),  # Diffuse
+            np.array([0.8, 0.7, 0.2, 1.0]),  # Specular
+            83
+            )
+        ellipsoid.setMaterial(m_gold)
+        ellipsoid.renderingRouting = "lighting"
+        self.addChild(ellipsoid)
 
         torus = Component(Point((0.75, 0, -0.75)), DisplayableTorus(shaderProg, 0.15, 0.3, 36, 36, ColorType.SOFTRED))
         m2 = Material(np.array((0.1, 0., 0., 1.0)), np.array((0.3, 0.05, 0.05, 1)),
